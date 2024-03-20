246f1dde4dcf957c6a070103801ba06cc1854bc9
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2024-03-20 22:15:19 +0545

    chore: husky config

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,7 @@
-import { z } from "zod";
+import { z } from 'zod'
 
 const EnvSchema = z.object({
-  PORT: z.coerce.number(),
-});
+	PORT: z.coerce.number().positive().nonnegative().default(8000)
+})
 
-export const env = EnvSchema.parse(process.env);
+export const env = EnvSchema.parse(process.env)
--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,6 @@ import { env } from './config/env'
 
 const app = express()
 
-app.listen(env.PORT)
+app.get('/', (req, res) => res.send('Hello'))
+
+app.listen(env.PORT, () => console.log(`Server running on PORT: ${env.PORT}`))
