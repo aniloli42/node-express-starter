a9b7b93bfacb101da6cdf60cb07e366d684436df
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2024-03-20 22:26:29 +0545

    refactor: logger moved

---
+++ src/lib/logger.ts
@@ -0,0 +1,22 @@
+import { createLogger, transports, format } from 'winston'
+
+const logFormat = format.combine(
+	format.timestamp({ format: 'ddd DD-MM-YYYY hh:mm:ss A' }),
+	format.printf(
+		(info) =>
+			`[${info.timestamp}] - [${info.level.toUpperCase()}] - ${
+				info.message
+			}`
+	)
+)
+
+export const logger = createLogger({
+	level: 'info',
+	format: logFormat,
+	transports: [
+		new transports.Console(),
+		new transports.File({
+			filename: 'app.log'
+		})
+	]
+})
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import 'dotenv/config'
 import express from 'express'
-import { env } from './config/env'
+import { env } from '@config/env'
+import { logger } from '@lib/logger'
 
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello'))
 
-app.listen(env.PORT, () => console.log(`Server running on PORT: ${env.PORT}`))
+app.listen(env.PORT, () => logger.info(`Server running on PORT: ${env.PORT}`))
